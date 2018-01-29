2a2cc9e5df1bf6a8b2d9ba557a757660f3561060
David Kambersky <david.kambersky@gmail.com>
2018-01-29 10:36:17 +0000

    Basic functionality
    Allows the user to issue turn and move commands to the robot.

--- app/src/main/java/io/github/dkambersky/ktapp/MainActivity.kt
+++ app/src/main/java/io/github/dkambersky/ktapp/MainActivity.kt
@@ -2,14 +2,49 @@ package io.github.dkambersky.ktapp
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import khttp.post
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.experimental.launch
 
 class MainActivity : AppCompatActivity() {
+    val angleStep = 45.0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        button.text = "Test"
-        button.setOnClickListener { _ -> println("There is no spoon")}
+
+        buttonOnOff.setOnClickListener { onOffPressed() }
+        buttonLeft.setOnClickListener { rightPressed() }
+        buttonRight.setOnClickListener { leftPressed() }
+
+    }
+
+    var onOff = 0
+    var angle = 0.0
+
+    private fun onOffPressed() {
+        onOff = if (onOff == 0) 1 else 0
+        sendCommand()
+    }
+
+    private fun rightPressed() {
+        angle -= angleStep
+        sendCommand()
+    }
+
+    private fun leftPressed() {
+        angle += angleStep
+        sendCommand()
+    }
+
+    private fun sendCommand() {
+        launch {
+            post("http://robotix.james-odonnell.com/post",
+                    params = mapOf(
+                            "onOff" to onOff.toString(),
+                            "turnAngle" to angle.toString()
+                    )
+            )
+        }
     }
 }
