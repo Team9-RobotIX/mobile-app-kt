e386a23bd2a16957a431030f3b54cf298862ba80
David Kambersky <david.kambersky@gmail.com>
2018-02-09 13:13:36 +0000

    Request camera permissions at runtime
    Closes #11

--- app/src/main/java/io/github/dkambersky/ktapp/activities/ScanningActivity.kt
+++ app/src/main/java/io/github/dkambersky/ktapp/activities/ScanningActivity.kt
@@ -1,6 +1,10 @@
 package io.github.dkambersky.ktapp.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.widget.Toast
 import io.github.dkambersky.ktapp.R
 import io.github.dkambersky.ktapp.util.BaseScannerViewEventListener
@@ -14,8 +18,14 @@ class ScanningActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_scanning)
 
+        /* Check camera permissions */
+        ensurePermissions()
+
         /* Set up scanner */
         camview.startScanner()
+
+        camview.setHudVisible(false)
+
         camview.scannerViewEventListener = object : BaseScannerViewEventListener(this) {
             override fun onCodeScanned(data: String) {
 
@@ -27,6 +37,12 @@ class ScanningActivity : BaseActivity() {
                     launch { readFromServer("/lock") }
             }
         }
+    }
+
+    private fun ensurePermissions() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA)
+                == PackageManager.PERMISSION_DENIED)
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 50)
     }
 
 }
